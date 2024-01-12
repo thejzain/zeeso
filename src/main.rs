42f9e5388197d08fa8269d9b2f39404bbe0a4fbe
thejzain <thejzainkp@gmail.com>
2024-01-12 23:20:46 +0530

    panic msg

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("panic here ");
     loop {}
 }
